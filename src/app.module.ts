b81eec88d160c1ca2ff1e8a0a54f723fb8d89c07
Dmitriy Falko <d.falko@medpoint24.ru>
2020-10-19 19:47:51 +0300

    Add fronend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { StationModule } from './domains/station';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), StationModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+    StationModule
+  ],
 })
 export class AppModule {}
